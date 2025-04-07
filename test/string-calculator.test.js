5a9a7309f083235f10de6e476d80467e79c5b896
Advait Maindalkar <maindalkar.advait@gmail.com>
2025-04-07 12:46:18 +0530

    writing test to check case with just one number

--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -1,8 +1,13 @@
 const add = require('../src/string-calculator.js')
 
-describe('String Calculator', () => { 
-  it('should return 0 for an emtpy string', () => {
+describe('String Calculator', () => {
+  it('should return 0 for an empty string', () => {
     const result = add('')
     expect(result).toBe(0)
+  })
+
+  it('should return the same number for a string with one number', () => {
+    const result = add('1')
+    expect(result).toBe(1)
   })
 })

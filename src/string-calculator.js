d229a8536a76a3f5b1c460617266c57b34150450
Advait Maindalkar <maindalkar.advait@gmail.com>
2025-04-07 12:46:32 +0530

    handling the case when string has just one number

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,6 +1,10 @@
-const add = (string) => {
-  if (string === '') {
+const add = (stringOfNumbers) => {
+  if (stringOfNumbers === '') {
     return 0
+  }
+
+  if (stringOfNumbers.length === 1) {
+    return parseInt(stringOfNumbers)
   }
 }
 

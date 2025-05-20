cbdbed528e4ae470c5056110094d360567ac638c
Advait Maindalkar <maindalkar.advait@gmail.com>
2025-05-20 15:23:12 +0530

    refactoring the code for readability

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,3 +1,9 @@
+/**
+ * Adds numbers from a string with specified delimiter formats
+ * @param {string} stringOfNumbers - String containing numbers separated by delimiters, optionally specifying custom delimiters in the format: //[delimiters]\n[numbers], or //[delimiter1][delimiter2]\n[numbers] for multiple delimiters
+ * @returns {number} Sum of the numbers
+ * @throws {Error} When input contains negative numbers or alphabets
+ */
 const add = (stringOfNumbers) => {
   // In case of empty string return 0
   if (stringOfNumbers === '') {
@@ -10,53 +16,76 @@ const add = (stringOfNumbers) => {
   }
 
   // handling custom delimiters (string is in the format: //[delimiter]\n[numbers])
+  let { delimiter, stringWithoutInitialDelimiter } = _parseDelimiter(stringOfNumbers)
+  const listOfNumbers = _splitStringToNumbers(stringWithoutInitialDelimiter, delimiter)
+  const sum = _calculateSum(listOfNumbers)
+
+  return sum
+}
+
+const _parseDelimiter = (stringOfNumbers) => {
   let delimiter = ','
-  const customDelimiterMatch = stringOfNumbers.match(/\/\/(.*)\n/)
 
-  if (customDelimiterMatch?.length > 0) {
-    // for single character delimiters
-    delimiter = customDelimiterMatch[1]
+  const CUSTOM_DELIMITER_CHECK_REGEX = /\/\/(.*)\n/
+  const customDelimiters = stringOfNumbers.match(CUSTOM_DELIMITER_CHECK_REGEX)
 
-    const multiCharacterDelimiterMatch = delimiter.match(/\[(.*)\]/)
-    // for multi-character delimiters
-    if (multiCharacterDelimiterMatch?.length > 0) {
-      delimiter = multiCharacterDelimiterMatch[1]
-    }
+  const customDelimiterExists = customDelimiters?.length > 0
+  if (!customDelimiterExists)
+    return { delimiter, stringWithoutInitialDelimiter: stringOfNumbers }
 
-    // for multiple delimiters with arbitrary length
-    if (delimiter.includes('][')) {
-      delimiter = delimiter.split('][')
-    }
+  // for single character custom delimiters
+  delimiter = customDelimiters[1]
 
-    stringOfNumbers = stringOfNumbers.substring(stringOfNumbers.indexOf('\n') + 1)
+  // for multi-character custom delimiters
+  const MULTI_CHARACTER_DELIMITER_CHECK_REGEX = /\[(.*)\]/
+  const multiCharacterDelimiterMatch = delimiter.match(MULTI_CHARACTER_DELIMITER_CHECK_REGEX)
+  if (multiCharacterDelimiterMatch?.length > 0) {
+    delimiter = multiCharacterDelimiterMatch[1]
   }
 
+  // for multiple delimiters with arbitrary length
+  const multipleDelimiterSplitter = '][';
+  if (delimiter.includes(multipleDelimiterSplitter)) {
+    delimiter = delimiter.split(multipleDelimiterSplitter)
+  }
+
+  const stringWithoutInitialDelimiter = stringOfNumbers.substring(stringOfNumbers.indexOf('\n') + 1)
+  return { delimiter, stringWithoutInitialDelimiter }
+}
+
+const _splitStringToNumbers = (stringOfNumbers, delimiter) => {
   // normalize delimiters (`\n` to `,`)
   stringOfNumbers = stringOfNumbers.replaceAll('\n', ',')
 
   // normalize other delimiter(s) and then split
+  const singleDelimiterCheck = typeof delimiter === 'string'
   let listOfStringOfNumbers;
-  if (typeof delimiter === 'string') {
+  if (singleDelimiterCheck) {
     listOfStringOfNumbers = stringOfNumbers.replaceAll(delimiter, ',').split(',')
   } else {
     delimiter.forEach(d => stringOfNumbers = stringOfNumbers.replaceAll(d, ','))
     listOfStringOfNumbers = stringOfNumbers.split(',')
   }
 
+  return listOfStringOfNumbers
+}
 
-  // calculating sum while handling edge cases
-  const negatives = []
-  const sum = listOfStringOfNumbers.reduce((sum, number) => {
-    if (/[a-zA-Z]/.test(number))
+// calculating sum while handling edge cases
+const _calculateSum = (listOfNumbers) => {
+  const MAX_ALLOWED_NUMBER = 1000
+  const listOfNegativeNumbers = []
+  const sum = listOfNumbers.reduce((sum, number) => {
+    const ALPHABET_REGEX = /[a-zA-Z]/
+    if (ALPHABET_REGEX.test(number))
       throw new Error(`Alphabets are not allowed`)
 
     number = parseInt(number)
     if (number < 0) {
-      negatives.push(number)
+      listOfNegativeNumbers.push(number)
       return sum
     }
 
-    if (isNaN(number) || number > 1000) {
+    if (isNaN(number) || number > MAX_ALLOWED_NUMBER) {
       return sum
     }
     
@@ -64,8 +93,8 @@ const add = (stringOfNumbers) => {
   }, 0)
 
   // throwing an error if there are negative numbers
-  if (negatives.length > 0) {
-    throw new Error(`Negative numbers not allowed: ${negatives.join(', ')}`)
+  if (listOfNegativeNumbers.length > 0) {
+    throw new Error(`Negative numbers not allowed: ${listOfNegativeNumbers.join(', ')}`)
   }
 
   return sum
